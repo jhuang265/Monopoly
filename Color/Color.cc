9060e873081b8cb70ad041db3f509384753e682b
Jerry Huang <jerryhuang265@hotmail.com>
2019-07-05 22:08:37 -0400

    fixing colors

--- Color/Color.cc
+++ Color/Color.cc
@@ -3,10 +3,32 @@ using namespace std;
 
 Color:: Color(){}
 
-void Color:: updateCanBuild(){
+void Color::updateCanBuild(){
+    /*
     if(properties[0].getOwnerIndex() == properties[1].getOwnerIndex() && properties[1].getOwnerIndex() == properties[2].getOwnerIndex()  ){
         for(int x =0; x<properties.size();x++){
             properties[x].owner.setCanBuild();
         }
     }
+    */
+    int index = properties[0].getOwnerIndex();
+    
+    for(int i = 0; i < properties.size(); ++i){
+        properties[i].setCanBuild();
+    }
+
+    bool setBuild = true;
+
+    for (int i = 1; i < properties.size(); ++i){
+        if(properties[i].getOwnerIndex != i){
+            setBuild = false;
+        }
+    }
+
+    if(setBuild){
+        for(int i = 0; i < properties.size(); ++i){
+            properties[i].setCanBuild()
+        }
+    }
+
 }
