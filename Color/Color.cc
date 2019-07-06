33f81db5ae1803b897976442db4a47db2bdd3ec6
Jerry Huang <jerryhuang265@hotmail.com>
2019-07-06 04:21:20 -0400

    color

--- Color/Color.cc
+++ Color/Color.cc
@@ -6,25 +6,25 @@ using namespace std;
 Color::Color(){}
 
 void Color::updateCanBuild(){
-    int index = properties[0].getOwnerIndex();
+    int index = properties[0]->getOwnerIndex();
     
-    bool setBuild = properties[0].getCanBuild();
+    bool setBuild = properties[0]->getCanBuild();
 
     if(setBuild){
         for(int i = 0; i < properties.size(); ++i){
-            properties[i].setCanBuild();
+            properties[i]->setCanBuild();
         }
     }
 
     for (int i = 1; i < properties.size(); ++i){
-        if(properties[i].getOwnerIndex != i){
+        if(properties[i]->getOwnerIndex() != i){
             setBuild = false;
         }
     }
 
     if(setBuild){
         for(int i = 0; i < properties.size(); ++i){
-            properties[i].setCanBuild()
+            properties[i]->setCanBuild();
         }
     }
 }
