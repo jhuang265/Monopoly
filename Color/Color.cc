5a2d58e1b5173f811c6741f53ecbc2c4e74931c8
Jerry Huang <jerryhuang265@hotmail.com>
2019-07-05 22:30:48 -0400

    fix color switching

--- Color/Color.cc
+++ Color/Color.cc
@@ -4,20 +4,15 @@ using namespace std;
 Color:: Color(){}
 
 void Color::updateCanBuild(){
-    /*
-    if(properties[0].getOwnerIndex() == properties[1].getOwnerIndex() && properties[1].getOwnerIndex() == properties[2].getOwnerIndex()  ){
-        for(int x =0; x<properties.size();x++){
-            properties[x].owner.setCanBuild();
-        }
-    }
-    */
     int index = properties[0].getOwnerIndex();
     
-    for(int i = 0; i < properties.size(); ++i){
-        properties[i].setCanBuild();
-    }
+    bool setBuild = properties[0].getCanBuild();
 
-    bool setBuild = true;
+    if(setBuild){
+        for(int i = 0; i < properties.size(); ++i){
+            properties[i].setCanBuild();
+        }
+    }
 
     for (int i = 1; i < properties.size(); ++i){
         if(properties[i].getOwnerIndex != i){
@@ -30,5 +25,4 @@ void Color::updateCanBuild(){
             properties[i].setCanBuild()
         }
     }
-
 }
