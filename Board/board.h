661adadd27fbf77e2f4c376fb63e0b48ed3592f2
Jerry Huang <jerryhuang265@hotmail.com>
2019-07-09 21:49:05 -0400

    comments

--- Board/board.h
+++ Board/board.h
@@ -15,26 +15,36 @@
 
 class Board{
     private:
+        // We assign all the tiles to the board
         std::vector<std::shared_ptr<Tile>> tiles;
         std::vector<std::shared_ptr<Color>> colors;
         std::vector<std::shared_ptr<Property>> properties;
         std::vector<std::shared_ptr<Utility>> utilities;
         std::vector<std::shared_ptr<Transportation>> transportations;
+        // Game type and number of players
         int type;
         int numPlayers;
+        // Keeping track of whose turn it is
         int currentPlayer;
+        // Keep track of active players
         std::vector<std::shared_ptr<Player>> players;
         //std::vector<shared_ptr<Card>> cards;
     public:
         Board(int type, int players);
         //void printCurPlayerStatus();
+        // Trading function
         void trade(shared_ptr<Player>);
+        // Playing turn
         int rollDice();
         void rollDiceAndAction();
         void playTurn();
         void checkOwnership();
+
+        // Auction function
         void auction(std::shared_ptr<Tile>);
         std::string getAssetType(std::string);
+
+        // Display board to user
         void printBoard();
 
         ~Board();
