3e06dd2afb29cd9895021991f5254d058ddbec9b
Shawn Xie <shawnxie920@gmail.com>
2019-07-12 00:02:17 -0400

    add back file

---
+++ Card/loseMoneyCard.h
@@ -0,0 +1,14 @@
+#ifndef __loseMoneyCard_H__
+#define __loseMoneyCard_H__
+#include "card.h"
+
+class loseMoneyCard {
+ vector<string> desc;
+
+ public:
+  loseMoneyCard();
+  void use(shared_ptr<Player>);
+  ~loseMoneyCard();
+};
+
+#endif
