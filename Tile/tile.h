b406dc6a92f13e57e2752b5e4460a7568691e869
Shawn Xie <shawnxie920@gmail.com>
2019-07-13 23:10:24 -0400

    comments

--- Tile/tile.h
+++ Tile/tile.h
@@ -7,36 +7,53 @@ class Player;
 
 class Tile{
     private:
+        //stores the type of type of the tile (Ex. Property, Transportation, Utility, Jail, Free Parking)
         std::string tileType;
+
+        //stores the actual name of the asset
         std::string name;
     public:
+        //return if the asset is bought
         virtual bool getIsOwned(){
             return false;
         };
+        //returns an integer indicating if the property can build house on
         virtual int getCanBuild(){
             return 0;
         };
+        //returns the rent of the asset
         virtual int getRent(){
             return 0;
         };
+        //returns the rent of the utility given the the dice roll
         virtual int getUtilityRent(int){
             return 0;
         };
+        //returns the price to buy the asset
         virtual int getPrice(){
             return 0;
         };
+        //returns the owner of the asset as an index on the board
         virtual int getOwnerIndex(){
             return 0;
         };
+        //returns the number of houses built already
         virtual int getHouses(){
             return 0;
         };
+        //buys the house on a Property tile
         virtual void buyHouse(){
             return;
         };
         Tile(std::string, std::string);
+
+        //returns the type of the tile
         std::string getType();
+
+        //returns the name of tile
         std::string getName();
+
+        //gets the color of the Property
         virtual std::string getColor();
 
         //This function is callled when player wants to buy an asset
